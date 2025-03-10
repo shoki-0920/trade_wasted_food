81be909310a64a672e851a60ae568f0344eb276e
Your Name <ueharattyo920@gmail.com>
2025-03-10 17:48:20 +0900

    Remove public and node_modules from git tracking

--- public/assets/manifest-81286f62.js
+++
@@ -1,5 +0,0 @@
-//= link_tree ../images
-//= link_tree ../builds
-//= link application.js
-//= link application.tailwind.css//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
