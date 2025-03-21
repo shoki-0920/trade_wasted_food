1e69fc0f677ac39b95b2f59a13ff72e730abf0a5
Your Name <ueharattyo920@gmail.com>
2025-03-22 00:50:38 +0900

    fix redis environment

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,15 @@
-Redis.new(url: ENV["REDIS_URL"])
+# config/initializers/redis.rb
+
+if Rails.env.production?
+  # ✅ 本番環境（Upstash）
+  Redis.current = Redis.new(
+    url: ENV['UPSTASH_REDIS_URL'],     # rediss://... 形式
+    password: ENV['REDIS_PASSWORD']    # Fly.io に登録した秘密
+  )
+else
+  # ✅ 開発環境（ローカル Redis）
+  redis_url = ENV['REDIS_URL'] || "redis://localhost:6379/0" # .envに設定したREDIS_URLを使う
+  Redis.current = Redis.new(
+    url: redis_url    # docker-composeなどで起動するRedis
+  )
+end
