631c6099121c6b528384d33641680b4e8b804e99
Your Name <ueharattyo920@gmail.com>
2025-03-24 21:54:43 +0900

    fixed test

--- test/controllers/health_controller_test.rb
+++ test/controllers/health_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class HealthControllerTest < ActionDispatch::IntegrationTest
   test "should get show" do
-    get health_show_url
+    get health_url
     assert_response :success
   end
 end
