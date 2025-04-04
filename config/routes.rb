faf2e1d60543527705f882f92c370016d2f6780f
Your Name <ueharattyo920@gmail.com>
2025-04-05 00:34:39 +0900

    fix lint

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get "health" => "health#show"
   resources :users, only: [ :edit, :update ]
   resource :profile, only: [ :edit, :update ]  # プロフィール編集ページ
-  resources :posts, only: [:index] #投稿管理
+  resources :posts, only: [ :index ] # 投稿管理
   # Auth0コントローラーのルート
   get "/auth/auth0/callback", to: "auth0#callback"
   get "/auth/failure", to: "auth0#failure"
