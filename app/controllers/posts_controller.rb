a97939fe28450089bd5eb7163c9ff40051834526
Your Name <ueharattyo920@gmail.com>
2025-04-05 22:46:45 +0900

    newpost #9

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,24 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all.order(created_at: :desc) # 新しい投稿を上に表示
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to posts_path, notice: '投稿完了しました'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :description, :price, :image)
+  end
 end
