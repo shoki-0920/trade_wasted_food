9002f26ef80bd4d99274459e1eb577aa9c004f62
Your Name <ueharattyo920@gmail.com>
2025-04-24 23:26:33 +0900

    fix_ui_posts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
+// tailwind.config.js
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',  // app/javascript に変更
+    './app/javascript/**/*.js',
     './app/assets/stylesheets/**/*.css'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        lobster: ['Lobster', 'cursive'],
+        water: ['"Water Brush"', 'cursive'],
+      },
+      keyframes: {
+        fadeInUp: {
+          '0%': { opacity: 0, transform: 'translateY(20px)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
+      animation: {
+        fadeInUp: 'fadeInUp 0.8s ease-out forwards',
+      },
+    },
   },
   plugins: [],
 };
