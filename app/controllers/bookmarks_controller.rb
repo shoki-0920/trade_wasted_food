f1ebe0bb37408cb7ad1d043f13c3dd6e8040b2a6
上原聖輝 <UEHARASHOKI@MacBook-Air.local>
2025-06-11 22:20:05 +0900

    lint_fix

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,7 +2,7 @@ class BookmarksController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     current_user.bookmark(@post)
-    
+
     respond_to do |format|
       format.turbo_stream
       format.html { redirect_back(fallback_location: root_path) }
@@ -12,7 +12,7 @@ class BookmarksController < ApplicationController
   def destroy
     @post = Post.find(params[:post_id])
     current_user.unbookmark(@post)
-    
+
     respond_to do |format|
       format.turbo_stream
       format.html { redirect_back(fallback_location: root_path) }
